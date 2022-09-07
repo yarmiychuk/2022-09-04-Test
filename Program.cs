eeb1053376e25d7b652844826035e9877f114e42
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-09-08 02:11:21 +0300

    Programm Test added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Контрольная работа
+Написать программу, которая из имеющегося массива строк
+формирует новый массив строк, длина которых меньше либо равна 3 символам.
+*/
+
+Random rnd = new Random();
+
+string[] CreateArray()
+{
+    int size = rnd.Next(3, 10);
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToString(rnd.Next(0, 10000));
+    }
+    return array;
+}
+
+int GetNewSize(string[] array)
+{
+    int size = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3) size++;
+    
+    return size;
+}
+
+string[] CreateNewArray(string[] existArray)
+{
+    int size = GetNewSize(existArray);
+    string[] newArray = new string[size];
+    int index = 0;
+
+    for (int i = 0; i < existArray.Length; i++)
+        if (existArray[i].Length <= 3)
+        {
+            newArray[index] = existArray[i];
+            index++;
+        }
+
+    return newArray;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = CreateArray();
+Console.WriteLine("В имеющемся массиве:");
+ShowArray(array);
+
+Console.WriteLine("Элементы с длиной 3 и меньше символов:");
+string[] newArray = CreateNewArray(array);
+ShowArray(newArray);
+if (newArray.Length == 0)
+    Console.WriteLine("(отсутствуют)");
